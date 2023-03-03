23dcd9812418656b93877e2acec5402db2568522
VoidBlanc <angelic.noted_0g@icloud.com>
2023-03-03 22:22:28 +0800

    Added new code for left & right hand

--- app/app.py
+++ app/app.py
@@ -3,43 +3,42 @@ import cv2
 import mediapipe as mp
 
 app = Flask(__name__)
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+
 def generate():
-    camera = cv2.VideoCapture(0)
-    mpHands = mp.solutions.hands
-    hands = mpHands.Hands()
-    mpDraw = mp.solutions.drawing_utils
+    """Generator function to capture video frames and yield them as byte strings."""
+    cap = cv2.VideoCapture(0)  # Use 0 for default camera, or change to URL for IP camera
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while True:
+            ret, frame = cap.read()
 
-    #Cv2 text initialize
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    org = (50, 50)
-    fontScale = 1
-    color = (0, 0, 0)
-    thickness = 2
+            # Convert the BGR image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    while True:
-        success, image = camera.read()
-        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = hands.process(imageRGB)
+            # Pass the RGB image through the Holistic model to get body landmark detections
+            results = holistic.process(image)
 
-        if results.multi_hand_landmarks:
-            for handlandmarks in results.multi_hand_landmarks:
-                mpDraw.draw_landmarks(image, handlandmarks, mpHands.HAND_CONNECTIONS)
-                thumbx = handlandmarks.landmark[mpHands.HandLandmark.THUMB_TIP].x
-                indexx = handlandmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x
+            # Draw the body landmark detections on the image
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+            if results.left_hand_landmarks is not None:
+                print(f"lEFT ${results.left_hand_landmarks}")
 
-                # print("thumb: " + thumbx)
-                # print("index: " + indexx)
-                if thumbx > indexx:
-                    image = cv2.putText(image, 'I am a BAKA', org, font,
-                                        fontScale, color, thickness, cv2.LINE_AA)
-                    #cv2.putText(image, 'I am a baka.', (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            if results.right_hand_landmarks is not None:
+                print(f"right ${results.right_hand_landmarks}")
+            # Convert the BGR image back to RGB
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+            # Convert the image to a byte string and yield it to the web app
+            ret, buffer = cv2.imencode('.jpg', image)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
-        ret, buffer = cv2.imencode('.jpg', image)
-        frame = buffer.tobytes()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
